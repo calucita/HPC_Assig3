69f5969551bca3d55994a26ecfe91465b3273f04
NathanQuadrio <nathan.quadrio@gmail.com>
2015-01-23 11:00:45 +0100

    latest jacobi par

--- Jacobi/jacobi_par.cu
+++ Jacobi/jacobi_par.cu
@@ -6,8 +6,8 @@
 #include <helper_functions.h> 
 #include <sys/time.h>
 
-#define MAX_ITER 1000
-#define NODES 32 // boundaries included
+#define MAX_ITER 10000
+#define NODES 500 // boundaries included
 
 
 __global__ void kernel_jacobi0(double *u, double *u_old, double *u_d1, double *f, int N){
@@ -22,10 +22,6 @@ __global__ void kernel_jacobi0(double *u, double *u_old, double *u_d1, double *f
 	i = blockIdx.x * blockDim.x + threadIdx.x+1; //column
 
 	// computing solution
-	/*if (j <= N/2 && i <= N){ 
-		printf("dev 0 %5.0lf in thread %i, %i \n", u[i+j*N], j, i);
-		
-	}*/
 	
 	if (j < N/2-1 && i < N-1){		
 		u[i+j*N] = 0.25 * ( u_old[(i-1)+j*N] + u_old[(i+1)+j*N] + \
@@ -52,12 +48,7 @@ __global__ void kernel_jacobi1(double *u, double *u_old, double *u_d0, double *f
 	i = blockIdx.x * blockDim.x + threadIdx.x+1; //column
 
 	// computing solution
-	/*if (j <= N/2 && i <= N){ 
-		printf("dev 1 %5.0lf in thread %i, %i\n", u[i+j*N], j, i);
-	}*/
 	
-	//printf("%lf \n", u[i+j*N]);
-
 	if (j > 0 && j < N/2-1 && i < N-1){
 		
 		u[i+j*N] = 0.25 * ( u_old[(i-1)+j*N] + u_old[(i+1)+j*N] + \
@@ -67,8 +58,7 @@ __global__ void kernel_jacobi1(double *u, double *u_old, double *u_d0, double *f
 		
 		u[i+j*N] = 0.25 * ( u_old[(i-1)+j*N] + u_old[(i+1)+j*N]  + \
 				    u_d0[i+(N/2-1)*N] + u_old[i+(j+1)*N] + \
-			            h*h*f[i+j*N] );
-		//printf("%5.0lf \n", u_d0[i+(N/2-1)*N]);	
+			            h*h*f[i+j*N] );	
 	}
 	
 }
@@ -77,21 +67,30 @@ __global__ void kernel_jacobi1(double *u, double *u_old, double *u_d0, double *f
 int main(int argc, char *argv[])
 {
 
-	int N;// = NODES;
-	int sizeXGrid;// = 1;
-	int sizeXBlock;// = N-2; 
+	struct timeval t1, t2;
+	gettimeofday(&t1, NULL);
 
-	//if (argc == 2)
-	//{	
+	int N = NODES;
+	int sizeXGridx = 1;
+	int sizeXGridy = 1;
+	int sizeXBlockx = N-2;
+	int sizeXBlocky = N-2; 
+
+	if (argc == 2)
+	{	
 		N = NODES;//atoi(argv[1]);
-		if (N <= 24){ 
-			sizeXBlock = N-2;
-			sizeXGrid = 1;
+		if (N <= 18){ 
+			sizeXBlockx = N-2;
+			sizeXBlocky = (sizeXBlockx/2)+1;
+			sizeXGridx = 1;
+			sizeXGridy = sizeXGridx;
 		} else {	
-			sizeXBlock = 16; 
-			sizeXGrid = ((N-2)+sizeXBlock-1)/sizeXBlock;
+			sizeXBlockx = 16;
+			sizeXBlocky = sizeXBlockx; 
+			sizeXGridx = ((N-2)+sizeXBlockx-1)/sizeXBlockx;
+			sizeXGridy = (sizeXGridx/2)+1;
 		}
-	//}
+	}
 
 	// variables declaration
 	int i, j, k, max_iter;
@@ -100,14 +99,29 @@ int main(int argc, char *argv[])
 	double *u_d1, *u_old_d1, *f_d1;
 	double conv, *temp;
 
-	dim3 DimGrid(sizeXGrid,sizeXGrid);
-	dim3 DimBlock(sizeXBlock,sizeXBlock); // 484 threads per block 
+	dim3 DimGrid(sizeXGridx,sizeXGridy);
+	dim3 DimBlock(sizeXBlockx,sizeXBlocky); // 484 threads per block 
 
 	max_iter = MAX_ITER;
 	
 	// initializing stopwatches
 	StopWatchInterface *timeKer;
 	sdkCreateTimer(&timeKer);
+
+	StopWatchInterface *timeMem10; //time to allocate to host
+	StopWatchInterface *timeMem11;	
+	sdkCreateTimer(&timeMem10);
+	sdkCreateTimer(&timeMem11);
+
+	StopWatchInterface *timeMem20; //time to copy to device
+	StopWatchInterface *timeMem21;	
+	sdkCreateTimer(&timeMem20);
+	sdkCreateTimer(&timeMem21);
+
+	StopWatchInterface *timeMem30; //time to copy to host
+	StopWatchInterface *timeMem31;	
+	sdkCreateTimer(&timeMem30);
+	sdkCreateTimer(&timeMem31);
 
 	// allocating solution and forcing term in the host 
 	f_h     = (double *)malloc(N*N * sizeof(double));
@@ -135,30 +149,39 @@ int main(int argc, char *argv[])
 		} 
 	}
 	
+	
 	// set device 0 as current and allocating solution and forcing term
 	cudaSetDevice(0);
+	sdkStartTimer(&timeMem10);	
 	cudaMalloc((void**)&f_d0,(N*N/2) * sizeof(double));
 	cudaMalloc((void**)&u_d0,(N*N/2) * sizeof(double));
 	cudaMalloc((void**)&u_old_d0,(N*N/2) * sizeof(double));
+	sdkStopTimer(&timeMem10);
 
 	// copying from host to device 0
+	sdkStartTimer(&timeMem20);
 	cudaMemcpy(u_d0, u_h, (N*N/2)*sizeof(double),cudaMemcpyHostToDevice);
 	cudaMemcpy(u_old_d0,u_old_h, (N*N/2)*sizeof(double),cudaMemcpyHostToDevice);
 	cudaMemcpy(f_d0,f_h, (N*N/2)*sizeof(double),cudaMemcpyHostToDevice);
+	sdkStopTimer(&timeMem20);
 
 	// enable peer to peer access with device 1
 	cudaDeviceEnablePeerAccess(1,0);
 
 	// set device 1 as current and allocating solution and forcing term
 	cudaSetDevice(1);
+	sdkStartTimer(&timeMem11);
 	cudaMalloc((void**)&f_d1,(N*N/2) * sizeof(double));
 	cudaMalloc((void**)&u_d1,(N*N/2) * sizeof(double));
 	cudaMalloc((void**)&u_old_d1,(N*N/2) * sizeof(double));
+	sdkStopTimer(&timeMem11);
 
 	// copying from host to device 1
+	sdkStartTimer(&timeMem21);
 	cudaMemcpy(u_d1, u_h+(N*N/2), (N*N/2)*sizeof(double),cudaMemcpyHostToDevice);
 	cudaMemcpy(u_old_d1, u_old_h+(N*N/2), (N*N/2)*sizeof(double),cudaMemcpyHostToDevice);
 	cudaMemcpy(f_d1, f_h+N*N/2, (N*N/2)*sizeof(double),cudaMemcpyHostToDevice);
+	sdkStopTimer(&timeMem21);
 
 	// enable peer to peer access with device 0
 	cudaDeviceEnablePeerAccess(0,0);
@@ -187,20 +210,17 @@ int main(int argc, char *argv[])
 	}
 	sdkStopTimer(&timeKer);
 
-/*
-	// synchronize devices
-	cudaSetDevice(0);
-	cudaCheckErrors(cudaDeviceSynchronize());
-	cudaSetDevice(1);
-	cudaCheckErrors(cudaDeviceSynchronize());*/
-
 	// copying from device 0 to host
 	cudaSetDevice(0);
+	sdkStartTimer(&timeMem30);
 	cudaMemcpy(u_h,u_d0, N*N/2 *sizeof(double),cudaMemcpyDeviceToHost);
+	sdkStopTimer(&timeMem30);
 
 	// copying from device 1 to host
 	cudaSetDevice(1);
+	sdkStartTimer(&timeMem31);
 	cudaMemcpy(u_h+N*N/2,u_d1, N*N/2 *sizeof(double),cudaMemcpyDeviceToHost);
+	sdkStopTimer(&timeMem31);
 
 	// print solution
 
@@ -218,10 +238,8 @@ int main(int argc, char *argv[])
    	fclose(fp);	
 
 	// print time
-	double tK = sdkGetTimerValue(&timeKer);
-	printf("Kernel time: %f \n", tK/1e3);
-	printf("Block size: %i x %i \n", sizeXBlock,sizeXBlock);
-	printf("Grid size: %i x %i \n", sizeXGrid, sizeXGrid);
+	printf("Block size: %i x %i \n", sizeXBlockx,sizeXBlocky);
+	printf("Grid size: %i x %i \n", sizeXGridx, sizeXGridy);
 
 	// freeing memory	
 	free(u_old_h);
@@ -230,6 +248,35 @@ int main(int argc, char *argv[])
 	cudaFree(u_old_d0);
 	cudaFree(u_d0);
 	cudaFree(f_d0);
+
+	double tK = sdkGetTimerValue(&timeKer);
+
+	double tM10 = sdkGetTimerValue(&timeMem10);
+	double tM20 = sdkGetTimerValue(&timeMem20);
+	double tM30 = sdkGetTimerValue(&timeMem30);
+
+	double tM11 = sdkGetTimerValue(&timeMem11);
+	double tM21 = sdkGetTimerValue(&timeMem21);
+	double tM31 = sdkGetTimerValue(&timeMem31);
+
+
+	printf("Kernel time: %lf \n", tK/1e3);
+	printf("Memory time: %lf \n", (tM10 + tM20 + tM30 + tM11 + tM21 + tM31)/1e3);
+
+	//printf("Block size: %i x %i \n", sizeXBlock,sizeXBlock);
+	//printf("Grid size: %i x %i \n", sizeXGrid, sizeXGrid);
+
+	double gputime = (tK + tM10 + tM20 + tM30 + tM11 + tM21 + tM31)/1e3;
+
+	gettimeofday(&t2, NULL);
+	double  walltime = t2.tv_sec - t1.tv_sec + (t2.tv_usec -t1.tv_usec) / 1.e6;
+	double cputime = walltime - gputime;
+	printf("Wall time: %lf \n", walltime);
+	printf("CPU time: %lf \n", cputime);
+
+	double kerneltime = (tK/2)/max_iter;
+	double flops = ((N*N*5)/1e9)/(kerneltime/1e3);
+	printf("Performance [GFlop/s]: %lf \n", flops);
 	
 	return 0;
 
